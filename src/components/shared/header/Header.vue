5d08da68682e01ac7f9041417f9f3e67097f7cfc
Danilo Scipioni <daniloedu@gmail.com>
2021-06-20 09:12:19 -0300

    Ajsute da tela de layout para mobile

--- src/components/shared/header/Header.vue
+++ src/components/shared/header/Header.vue
@@ -2,27 +2,27 @@
   <header class="cabecalho">
     <nav class="menu">
       <ul class="menu-lista">
-           <li class="menu-item" v-for="rota in rotas">
-          <router-link :to="rota.path ? rota.path : '/'" class="menu-link"  > {{$t(rota.title)}}</router-link>
-           </li>
+        <li class="menu-item" v-for="rota in rotas" :key="rota.path">
+          <router-link :to="rota.path ? rota.path : '/'" class="menu-link">
+            {{ $t(rota.title) }}</router-link
+          >
+        </li>
       </ul>
-
     </nav>
-    <locale-changer/>
+    <locale-changer />
   </header>
 </template>
 
 <script>
-
 import ChangeLang from '../select/ChangeLang.vue'
 
 export default {
-components: {
+  components: {
     'locale-changer': ChangeLang
   },
   data () {
     return {
-      active: false,
+      active: false
     }
   },
 
@@ -32,22 +32,20 @@ components: {
       required: true
     }
   }
-
 }
 </script>
 
 <style scope>
-
-.cabecalho{
-    align-items: center;
-    background: #f9f9f9;
-    display: flex;
-    justify-content: space-between;
-    padding-left: 2rem;
-    padding-right: 2rem;
+.cabecalho {
+  align-items: center;
+  background: #f9f9f9;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 2rem;
+  padding-right: 2rem;
 }
 
-.menu{
+.menu {
   display: flex;
 }
 
@@ -58,8 +56,6 @@ components: {
 .menu-item {
   list-style: none;
 }
-
-
 
 .menu-item:last-child a {
   margin-right: 0;
@@ -72,58 +68,64 @@ components: {
   text-transform: capitalize;
 }
 
-.router-link-exact-active{
- color: #333333;
- font-weight: bold;
+.router-link-exact-active {
+  color: #333333;
+  font-weight: bold;
 }
 
 .menu-link:hover {
   text-decoration: underline;
 }
 
-
 @media screen and (min-width: 0) {
-    .menu-lista {
-        display: block;
-        text-align: center;
-    }
+  .menu-lista {
+    display: block;
+    text-align: center;
+  }
 }
 
 @media screen and (min-width: 768px) {
-    .menu-lista {
-        display: flex;
-        
-    }
+  .menu-lista {
+    display: flex;
+  }
 }
 
 @media screen and (min-width: 0) {
-    .menu {
-       display: block;
-       width: 100%;
-       height: 150px;
-    }
+  .menu {
+    display: block;
+    width: 100%;
+    height: 150px;
+  }
 }
 
 @media screen and (min-width: 768px) {
-    .menu {
-        display: flex;
-        height: auto;
-        
-    }
+  .menu {
+    display: flex;
+    height: auto;
+  }
 }
 
 @media screen and (min-width: 0) {
-    .menu-item {
+  .menu-item {
     list-style: none;
     padding: 5px;
-    }
+  }
 }
 
 @media screen and (min-width: 768px) {
-    .menu-item {
-     list-style: none;
-    }
+  .menu-item {
+    list-style: none;
+  }
 }
-
-
+@media screen and (max-width: 768px) {
+  .cabecalho {
+    padding-bottom: 0.7em;
+  }
+  .menu-link {
+    color: #808080;
+    margin-right: 0rem;
+    text-decoration: none;
+    text-transform: capitalize;
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
 import { routes } from './routes'
 import i18n from './i18n'
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 // import 'bootstrap/dist/css/bootstrap.css'
@@ -14,9 +14,9 @@ Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 // Make BootstrapVue available throughout your project
-//Vue.use(BootstrapVue)
+// Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
-//Vue.use(IconsPlugin)
+// Vue.use(IconsPlugin)
 
 const router = new VueRouter(
   {
@@ -26,7 +26,8 @@ const router = new VueRouter(
 )
 
 Vue.use(VueResource)
-
+console.log(process.env)
+Vue.http.options.root = process.env.API_URL ? process.env.API_URL : 'http://localhost:3000'
 new Vue({
   router,
   i18n,
