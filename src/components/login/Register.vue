eaef0c427c7b9b8e0f3c8836dd2975d6358bccb7
Danilo Scipioni <danilo.scipioni@schott.com>
2021-06-21 10:53:55 -0300

    ajuste de layout da tela de login e registro de usuário

--- src/components/login/Register.vue
+++ src/components/login/Register.vue
@@ -1,11 +1,24 @@
 <template>
-  <form class="register-form" v-if="!signin">
+  <form class="register-form">
     <input type="text" placeholder="name" />
     <input type="password" placeholder="password" />
     <input type="text" placeholder="email address" />
     <button>create</button>
     <p class="message">
-      Already registered? <a href="#" v-on:click="this.$emit('register')">Sign In</a>
+     {{ $t('login-register.is-registered') }} <a href="#" @click="register">{{ $t('login-register.signin') }}</a>
     </p>
   </form>
 </template>
+<script>
+export default {
+  name: 'Register',
+  methods: {
+    register (item) {
+      this.$emit('registerOrSignin')
+    }
+  }
+}
+</script>
+<style scoped lang="sass">
+  @import "./styles/login-register.scss";
+</style>
--- src/components/login/Sigin.vue
+++ src/components/login/Sigin.vue
@@ -1,10 +1,29 @@
 <template>
+
   <form class="login-form">
-    <input type="text" placeholder="username" />
-    <input type="password" placeholder="password" />
+
+    <input type="text" :placeholder = "$t('login-register.username')" />
+    <input type="password" :placeholder="$t('login-register.password')" />
     <button>login</button>
     <p class="message">
-      Not registered? <a href="#" v-on:click="this.$emit('register')">Create an account</a>
+      {{ $t('login-register.not-registered') }} <a href="#" @click="signin">{{ $t('login-register.create-account') }}</a>
     </p>
   </form>
 </template>
+<script>
+export default {
+  name: 'Signin',
+  data () {
+    return {
+    }
+  },
+  methods: {
+    signin () {
+      this.$emit('registerOrSignin')
+    }
+  }
+}
+</script>
+<style scoped lang="sass">
+  @import "./styles/login-register.scss";
+</style>
