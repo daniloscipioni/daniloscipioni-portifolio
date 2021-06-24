e2e7ddac3e3719207d143b95fa331f352fd1a81c
Danilo Scipioni <daniloedu@gmail.com>
2021-06-24 09:50:15 -0300

    Configuração vuex

--- src/main.js
+++ src/main.js
@@ -4,19 +4,12 @@ import VueResource from 'vue-resource'
 import VueRouter from 'vue-router'
 import { routes } from './routes'
 import i18n from './i18n'
-// import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-
-// Import Bootstrap an BootstrapVue CSS files (order is important)
-// import 'bootstrap/dist/css/bootstrap.css'
-// import 'bootstrap-vue/dist/bootstrap-vue.css'
+import http from '@/http'
+import store from '@/store'
 
 Vue.use(VueRouter)
 
 Vue.config.productionTip = false
-// Make BootstrapVue available throughout your project
-// Vue.use(BootstrapVue)
-// Optionally install the BootstrapVue icon components plugin
-// Vue.use(IconsPlugin)
 
 const router = new VueRouter(
   {
@@ -26,10 +19,13 @@ const router = new VueRouter(
 )
 
 Vue.use(VueResource)
-console.log(process.env)
+
 Vue.http.options.root = process.env.API_URL ? process.env.API_URL : 'http://localhost:3000'
+Vue.prototype.$http = http
+
 new Vue({
   router,
+  store,
   i18n,
   render: h => h(App)
 }).$mount('#app')
