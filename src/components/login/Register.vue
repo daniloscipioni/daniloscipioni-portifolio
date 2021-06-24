3f6785015b0c8b963c4fc4be07fb799d902e5b5b
Danilo Scipioni <daniloedu@gmail.com>
2021-06-24 09:49:50 -0300

    inclusao de espaco para se adequar o eslint

--- src/components/login/Register.vue
+++ src/components/login/Register.vue
@@ -2,7 +2,7 @@
   <form class="register-form">
     <input type="text" :placeholder="$t('login-register.username')" />
     <input type="password" :placeholder="$t('login-register.password')" />
-    <input type="text":placeholder="$t('login-register.email-address')" />
+    <input type="text" :placeholder="$t('login-register.email-address')" />
     <button>{{ $t('login-register.create') }}</button>
     <p class="message">
      {{ $t('login-register.is-registered') }} <a href="#" @click="register">{{ $t('login-register.signin') }}</a>
