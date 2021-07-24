30c7239e40f44a6b307cabd6fa95d0d21342e1e3
Danilo Scipioni <daniloedu@gmail.com>
2021-07-24 10:18:45 -0300

    Acerto de CSS do home com as noticias

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,26 +1,56 @@
 
 <template>
   <div>
-    {{updatedNews}}
-    <section class="home">
-      <meu-banner :url="url" :titulo="titulo" />
-      <div class="cards" v-for="item in news.valor" :key="item.date">
-        <meu-card :title=item.title :date=item.date :content=item.content :link=item.link source='Valor.com' />
+    {{ updatedNews }}
+    <div class="home">
+      <!-- <meu-banner :url="url" :titulo="titulo" /> -->
+      <div class="main-view">
+        <h1>Home</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos culpa
+        veritatis reprehenderit veniam, accusantium neque, itaque illo sit,
+        iusto maiores minima voluptatibus fugit perspiciatis assumenda quam
+        alias voluptas. Pariatur, consectetur.
       </div>
-      <div class="cards" v-for="item in news.g1" :key="item.date">
-        <meu-card :title=item.title :date=item.date :content=item.content :link=item.link source='G1.com' />
+      <div class="side-news">
+        <div class="cards" v-for="item in news.valor" :key="item.date">
+          <meu-card
+            :title="item.title"
+            :date="item.date"
+            :content="item.content"
+            :link="item.link"
+            source="Valor.com"
+          />
+        </div>
+        <div class="cards" v-for="item in news.g1" :key="item.date">
+          <meu-card
+            :title="item.title"
+            :date="item.date"
+            :content="item.content"
+            :link="item.link"
+            source="G1.com"
+          />
+        </div>
+        <div class="cards" v-for="item in news.wired" :key="item.date">
+          <meu-card
+            :title="item.title"
+            :date="item.date"
+            :content="item.content"
+            :link="item.link"
+            source="Wired.com"
+          />
+        </div>
+        <div class="cards" v-for="item in news.theguardian" :key="item.date">
+          <meu-card
+            :title="item.title"
+            :date="item.date"
+            :content="item.content"
+            :link="item.link"
+            source="TheGuardian.com"
+          />
+        </div>
       </div>
-      <div class="cards" v-for="item in news.wired" :key="item.date">
-        <meu-card :title=item.title :date=item.date :content=item.content :link=item.link source='Wired.com'/>
-      </div>
-      <div class="cards" v-for="item in news.theguardian" :key="item.date">
-        <meu-card :title=item.title :date=item.date :content=item.content :link=item.link source='TheGuardian.com' />
-      </div>
-      <h1>Home</h1>
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos culpa veritatis reprehenderit veniam, accusantium neque, itaque illo sit, iusto maiores minima voluptatibus fugit perspiciatis assumenda quam alias voluptas. Pariatur, consectetur.
-    </section>
-     </div>
-
+    </div>
+  </div>
 </template>
 
 <script>
@@ -36,30 +66,29 @@ export default {
     return {
       url: require('@/assets/images/banner_home.jpg'),
       titulo: 'Home',
-      news: [],
+      news: []
     }
   },
   computed: {
-      // uma função "getter" computada (computed getter)
+    // uma função "getter" computada (computed getter)
     updatedNews: function () {
       // `this` aponta para a instância Vue da variável `vm`
-    
+
       return this.updateNews()
     }
   },
-    watch: {
-      // sempre que a pergunta mudar, essa função será executada
+  watch: {
+    // sempre que a pergunta mudar, essa função será executada
     // news: function () {
     //   return this.updateNews()
     //   //this.debouncedGetAnswer()
     // },
-
-    },
+  },
   methods: {
     updateNews: function () {
-     
-          //this.fullName = val + ' ' + this.lastName
-           this.$store.dispatch('searchNews')
+      // this.fullName = val + ' ' + this.lastName
+      this.$store
+        .dispatch('searchNews')
         .then((data) => {
           // this.$router.push({ path: '/' })
           this.news = data.data
@@ -71,19 +100,52 @@ export default {
           // }
           console.log(err)
         })
-        
     }
-      //   return this.news;
-  //  }
+    //   return this.news;
+    //  }
   },
 
-  created () {
-
-  }
+  created () {}
 }
 </script>
 <style scoped>
-.cards{
+.home {
+  display: flex;
+  padding-left: 2rem;
+  padding-right: 2rem;
+}
+.main-view {
+  text-align: justify;
+  padding-right: 2rem;
+}
+.cards {
   display: inline-flex;
+}
+
+.side-news {
+  width: 35%;
+}
+
+@media screen and (min-width: 0) {
+  .home {
+    display: block;
+    width: 100%;
+    height: auto;
+  }
+  .main-view {
+    text-align: justify;
+    padding-right: 0rem;
+  }
+}
+
+@media screen and (min-width: 768px) {
+  .home {
+    display: flex;
+    height: auto;
+  }
+  .main-view {
+    text-align: justify;
+    padding-right: 2rem;
+  }
 }
 </style>
--- src/components/shared/card/Card.vue
+++ src/components/shared/card/Card.vue
@@ -2,14 +2,14 @@
   <div class="card-expansion">
     <md-card>
       <md-card-header>
-        <div class="md-title">{{title}}</div>
-        <div class="md-subhead">{{date}}</div>
+        <div class="md-title">{{ title }}</div>
+        <div class="md-subhead">{{ date }}</div>
       </md-card-header>
 
       <md-card-expand>
         <md-card-actions>
-         <md-card-expand-trigger >
-            <md-button class="md-icon-button" >
+          <md-card-expand-trigger>
+            <md-button class="md-icon-button">
               <md-icon>more_horiz</md-icon>
             </md-button>
           </md-card-expand-trigger>
@@ -17,38 +17,40 @@
 
         <md-card-expand-content>
           <md-card-content>
-           {{content}}
-           </md-card-content>
-           <div class="md-subhead">Fonte: 
-             <a :href=link target="_blank" >{{source}}</a>
-             </div>
+            {{ content }}
+          </md-card-content>
+          <div class="md-subhead">
+            Fonte:
+            <a :href="link" target="_blank">{{ source }}</a>
+          </div>
         </md-card-expand-content>
       </md-card-expand>
     </md-card>
   </div>
 </template>
 <script>
-  export default {
-    name: 'CardExpansion',
-    props:{
-      title: String,
-      date: String,
-      content: String,
-      link: String,
-      source: String
-    }
+export default {
+  name: 'CardExpansion',
+  props: {
+    title: String,
+    date: String,
+    content: String,
+    link: String,
+    source: String
   }
+}
 </script>
 
 <style lang="scss" scoped>
-  .card-expansion {
-    // height: 480px;
-  }
-
-  .md-card {
-    width: 320px;
-    margin: 4px;
-    display: inline-block;
-    vertical-align: top;
-  }
+.md-card {
+  width: 320px;
+  margin: 4px;
+  display: inline-block;
+  vertical-align: top;
+}
+.md-card .md-title {
+  font-size: 15px;
+  letter-spacing: 0;
+  line-height: 20px;
+}
 </style>
