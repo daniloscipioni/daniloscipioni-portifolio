a7e63ae04e5e973a09cda6342ffbac311c4c2715
Danilo Scipioni <daniloedu@gmail.com>
2022-04-29 18:58:14 -0300

    feat: add key for the routes

--- src/routes.js
+++ src/routes.js
@@ -7,10 +7,10 @@ import LoginAccess from './components/login/LoginAccess.vue'
 
 export const routes = [
 
-  { path: '', component: Home, title: 'header.home', meta: { public: true } },
-  { path: '/contact', component: Contact, title: 'header.contact' },
-  { path: '/skill', component: Skill, title: 'header.skill' },
-  { path: '/funcionalidades', component: Func, title: 'header.func' },
-  { path: '/login', component: LoginAccess, title: 'header.login', meta: { public: true }, login: { public: false } },
+  { path: '', component: Home, title: 'header.home', meta: { public: true }, key: 1 },
+  { path: '/contact', component: Contact, title: 'header.contact', key: 2 },
+  { path: '/skill', component: Skill, title: 'header.skill', key: 3 },
+  { path: '/funcionalidades', component: Func, title: 'header.func', key: 4 },
+  { path: '/login', component: LoginAccess, title: 'header.login', meta: { public: true }, login: { public: false }, key: 5 },
 
 ]
